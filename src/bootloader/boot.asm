c2b220dc45bc83da805490c92f742de7b811844d
AlexanderVinarsky <vinarskydichtv@gmail.com>
2026-01-07 12:29:07 +0300

    reading from the disk

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -43,7 +43,7 @@ start:
 	jmp main
 
 
-putc_strynges:
+puts:
 	push si
 	push ax
 
@@ -75,16 +75,148 @@ main:
     mov ss, ax
     mov sp, 0x7C00
 
-    mov si, msg_hello
-    call putc_strynges
-    
+	; read something from floppy disk
+	; BIOS should set DL to drive number
+	mov [ebr_drive_number], dl
 
+
+	mov ax, 1							; LBA=1, second sector from disk
+	mov cl, 1							; 1 sector to read
+	mov bx, 0x7E00						; data after the bootloader
+	call disk_read
+
+
+    mov si, msg_hello
+    call puts
+    
+	cli 								; disable interrupts, so CPU won't get out of the halt state
 	hlt
 
-.halt:
-	jmp .halt
+;
+; Error handlers
+;
 
-msg_hello: db 'Hello, world!', ENDL, 0
+floppy_error:
+	mov si, msg_read_failed
+	call puts
+	jmp wait_key_and_reboot
+
+wait_key_and_reboot:
+	mov ah, 0
+	int 16h								; wait for keypress
+	jmp 0FFFFFh:0						; jump to the begginning of BIOS, should reboot
+
+.halt:
+	cli 								; disable interrupts, so CPU won't get out of the halt state
+	hlt 
+
+
+
+;
+;	Disk routines
+;
+
+;
+; Converts an LBA address to a CHS (cylinder, head, sector) address
+; Parameters:
+;	-ax: LBA address
+; Returns:
+;	-cx [bits 0-5]:  sector number
+;	-cx [bits 6-15]: cylinder
+
+lba_to_chs:
+	
+	push ax
+	push dx
+
+	xor dx, dx							
+	div word [bdb_sectrors_per_track]	; ax = LBA / sectors_per_track
+										; dx = LBA % sectors_per_track
+										; dx = (LBA % sectors_per_track + 1) = sector
+	
+	inc dx								; dx = (LBA % sectors_per_track)
+	mov cx, dx							; cx = sector
+
+	xor dx, dx							; dx = 0
+	div word [bdb_heads]				; ax = (LBA / sectors_per_track) / heads = cylinder
+										; dx = (LBA / sectors_per_track) % heads = head
+	mov dh, dl							; dh = head
+	mov ch, al							; ch = cylinder (lower 8 bits)
+	shl ah, 6			
+	or cl, ah							; put upper 2 bits of cylinder in CL
+
+	pop ax
+	mov dl, al
+	pop ax
+	ret
+
+
+
+;
+; Reads sectors from the disk
+; Parameters:
+;	- ax: LBA address
+;	- cl: number of sectors to read (<=128)
+;	- dl: drive number
+;	- es:bx: memory address to store read data in
+;
+disk_read:
+	push ax								; saving registers before moifying
+	push bx
+	push cx
+	push dx
+	push di
+
+	push cx								; temporarily sa
+	call lba_to_chs						; compute CHS
+	pop ax								; AL - numbeer of sectors to read
+
+	mov ah, 02h
+	mov di, 3
+
+.retry:
+	pusha								; save all registers
+	stc 								; set carry flag
+	int 13h								; carry flag created -> success
+	jnc .done							; jump iff carry not set
+
+	; read failed
+	popa
+	call disk_reset
+
+	dec di 
+	test di, di
+	jnz .retry
+
+.fail:
+	jmp floppy_error
+
+.done:
+	popa
+
+	pop di								; saving registers before moifying
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	ret
+
+;
+; Resets disk controller
+; Params:
+;	dl: drive number
+;
+disk_reset:
+	pusha
+	mov ah, 0
+	stc
+	int 13h
+	jc floppy_error
+	popa
+	ret
+
+msg_hello: 				db 'Hello, world!', ENDL, 0
+msg_read_failed:	 	db 'Read from disk failed', ENDL, 0
 
 
 times 510-($-$$) db 0
